46c2edcdf1900f6567a758119c758b6b965cc37b
Dmytro <mityaua@gmail.com>
2021-01-09 21:00:22 +0200

    Fix naming

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './styles.css';
 import './js/theme';
-import './js/templating';
+import './js/create-cards';
--- src/js/templating.js
+++
@@ -1,7 +0,0 @@
-import postTemplate from '../templates/post-items.hbs';
-import menu from '../menu.json'
-
-const markup = postTemplate(menu);
-
-const menuRef = document.querySelector('.js-menu');
-menuRef.insertAdjacentHTML('beforeend', markup);
